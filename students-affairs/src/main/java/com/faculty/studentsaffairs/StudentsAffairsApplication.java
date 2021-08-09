8f7e2360fe33daab736b637c78b629ea8dd8b508
AbbasSajjad <abbasbanani94@gmail.com>
2021-08-09 19:43:49 +0300

    initiate spring boot files

---
+++ students-affairs/src/main/java/com/faculty/studentsaffairs/StudentsAffairsApplication.java
@@ -0,0 +1,13 @@
+package com.faculty.studentsaffairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsAffairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsAffairsApplication.class, args);
+	}
+
+}
---
+++ students-affairs/src/test/java/com/faculty/studentsaffairs/StudentsAffairsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculty.studentsaffairs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsAffairsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
